5f1395d0dfba53f8b0cf87a9b54d9fa3454b3778
tertullienchimi <tertullienkenmegne49@gmail.com>
2023-11-03 09:57:25 +0100

    exo2 ok

---
+++ Exercice2.py
@@ -0,0 +1,25 @@
+prix=[7,1,5,3,6,4]
+
+
+
+
+
+
+def maxProfit(prix):
+        gr=0
+        n=len(prix)
+        for i in range(n):
+            for j in range(0,(n-i-1)):
+                gr1=prix[j+1]-prix[j]
+                prix[j], prix[j+1] = prix[j+1], prix[j]
+                if (gr1 >=gr):
+                    gr=gr1
+
+        if (gr>=0):
+            print(gr)
+        else:
+            print("0")
+        return 0
+
+
+maxProfit(prix)
